6be94fdf2be019b8a21a634c78c61406dd8bc032
surajgoraicse <surajgoraicse@gmail.com>
2025-07-18 18:05:23 +0530

    router server setup

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,7 +3,18 @@ package main
 import (
 	"database/sql"
 	"log"
+
+	"github.com/surajgoraicse/go_event/internal/database"
+	"github.com/surajgoraicse/go_event/internal/env"
+	_ "github.com/mattn/go-sqlite3"
+	_ "github.com/joho/godotenv/autoload"
 )
+
+type application struct {
+	port      int
+	jwtSecret string
+	models    database.Models
+}
 
 func main() {
 	db, err := sql.Open("sqlite3", "./data.db")
@@ -11,5 +22,14 @@ func main() {
 		log.Fatal(err)
 	}
 	defer db.Close()
-	
+
+	models := database.NewModels(db)
+	app := &application{
+		port:      env.GetEnvInt("PORT", 8080),
+		jwtSecret: env.GetEnvString("JWT_SECRET", "some-secret-123234"),
+		models:    models,
+	}
+	if err := app.serve(); err != nil {
+		log.Fatal(err)
+	}
 }
